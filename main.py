e87a81f528a60f21347063d7bdfbf7aa003156a5
İclal Çetin <58071165+iclal07@users.noreply.github.com>
2024-09-06 07:44:29 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from scripts.train_model import train
+from scripts.test_model import test
+from scripts.evaluate_model import evaluate
+
+def main():
+    # Modeli eğit
+    train()
+
+    # Modeli test et
+    test()
+
+    # Modeli değerlendir
+    evaluate()
+
+if __name__ == "__main__":
+    main()
