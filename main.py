912ec50a9afdc665b309fa569164885880aca8fa
İclal Çetin <58071165+iclal07@users.noreply.github.com>
2024-09-26 21:49:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-from scripts.train_model import train
-from scripts.test_model import test
-from scripts.evaluate_model import evaluate
 
-def main():
-    # Modeli eğit
-    train()
-
-    # Modeli test et
-    test()
-
-    # Modeli değerlendir
-    evaluate()
-
-if __name__ == "__main__":
-    main()
